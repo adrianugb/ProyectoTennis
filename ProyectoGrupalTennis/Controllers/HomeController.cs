598c736efc62b517877833a34bdf540927f4a860
fabi-zamorao <bolitafabi@gmail.com>
2026-04-03 17:51:04 -0600

    Implementación módulo de autenticación (Login) + mejoras en homepage y slider con CTAs
    Módulo 1 — Seguridad y autenticación (Frontend)
     interfaz de usuario para el módulo de autenticación
    
    Se personalizó el slider principal para alinear el contenido con el propósito del sistema.
    
    Se agregaron botones en el slider para:
    
    Acceso al sistema (Login)
    Creación de cuenta (Registro)

--- ProyectoGrupalTennis/Controllers/HomeController.cs
+++ ProyectoGrupalTennis/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ namespace ProyectoGrupalTennis.Controllers
 {
     public class HomeController : Controller
     {
+        #region Vistas generales
+
         public IActionResult Index()
         {
             return View();
@@ -21,24 +23,52 @@ namespace ProyectoGrupalTennis.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        #endregion
+
+        #region Dashboard
+
         public IActionResult Dashboard()
         {
             return View("~/Views/Dashboard/Index.cshtml");
         }
+
+        #endregion
+
+        #region Notificaciones
 
         public IActionResult Notificaciones()
         {
             return View("~/Views/Notificaciones/Index.cshtml");
         }
 
+        #endregion
+
+        #region Feedback
+
         public IActionResult Feedback()
         {
             return View("~/Views/Feedback/Index.cshtml");
         }
 
+        #endregion
+
+        #region Progreso
+
         public IActionResult Progreso()
         {
             return View("~/Views/Progreso/Index.cshtml");
         }
+
+        #endregion
+
+        #region Seguridad y autenticación
+
+        public IActionResult Login()
+        {
+            return View("~/Views/Auth/Login.cshtml");
+        }
+
+        #endregion
     }
 }
